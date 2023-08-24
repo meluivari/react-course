6b8ee1993a0fbecdb1512459ddfe7c536f925436
Ovidiu Latcu <ovidiu.latcu@gmail.com>
2023-08-25 00:25:52 +0300

    chore: turnoff strict mode & add fake data

---
+++ src/utils/movies.ts
@@ -0,0 +1,33 @@
+export type Movie = {
+  id: number;
+  title: string;
+  img: string;
+  info?: string;
+  genre?: "action" | "fantasy" | "drama";
+};
+
+export const getMovies = (): Movie[] => [
+  {
+    id: 0,
+    title: "Star Wars IV: A new hope",
+    img: "https://i.imgur.com/LpVmm02.jpeg",
+  },
+  {
+    id: 1,
+    title: "Star Wars V: The Empire strikes back",
+    img: "https://imgur.com/klKfEvi",
+  },
+  {
+    id: 2,
+    title: "Star Wars V|: Return of the Jedi",
+    img: "https://i.imgur.com/0kV0pS5.jpeg",
+  },
+];
+
+export const getMoviesAsync = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(getMovies());
+    }, 800);
+  });
+};
