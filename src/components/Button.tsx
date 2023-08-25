ee5509c141bd3f842c049de59dd63b624673ba7a
Ovidiu Latcu <ovidiu.latcu@gmail.com>
2023-08-26 00:44:33 +0300

    chore: add missing Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import styles from "./button.module.css";
+
+type Props = {
+  label: string;
+  disabled?: boolean;
+  onClick: () => void;
+};
+
+export function Button({ label, onClick, disabled }: Props) {
+  return (
+    <button
+      className={
+        styles.buttonCommon +
+        ` ${disabled ? styles.buttonDisabled : styles.buttonActive}`
+      }
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <p>{label}</p>
+    </button>
+  );
+}
